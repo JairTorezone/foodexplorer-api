54ff56ffebc027bf61b9a291c672aa7939feb06f
Jair Torezone <jairtorezone@gmail.com>
2024-04-20 23:15:41 -0300

    created tables

---
+++ src/database/knex/migrations/20240421003127_createDish.js
@@ -0,0 +1,15 @@
+exports.up = (knex) =>
+  knex.schema.createTable("dish", (table) => {
+    table.increments("id");
+    table.text("name").notNullable();
+    table.text("image");
+
+    table.integer("category_id").references("id").inTable("category");
+
+    table.timestamp("created_at").default(knex.fn.now());
+    table.timestamp("updated_at").default(knex.fn.now());
+
+    table.float("price").notNullable();
+  });
+
+exports.down = (knex) => knex.schema.dropTable("dish");
