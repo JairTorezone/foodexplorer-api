a95f3d22ed31ec2389ad3075104d4eb8831409c3
Jair Torezone <jairtorezone@gmail.com>
2024-05-16 20:58:26 -0300

    created update user database

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UsersController {
   async create(request, response) {
@@ -21,6 +21,48 @@ class UsersController {
     await knex("users").insert({ name, email, password: hashedPassword });
 
     response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password, updated_at } = request.body;
+    const user_id = request.user.id;
+    console.log(user_id);
+
+    const user = await knex("users").where({ id: user_id }).first();
+
+    if (!user) {
+      throw new AppError("Usuário não cadastrado");
+    }
+
+    const checkEmailExist = await knex("users").where({ email: email }).first();
+
+    if (checkEmailExist && checkEmailExist.id !== user.id) {
+      throw new AppError("Email já esta cadastrado com outro usuário");
+    }
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir uma nova senha"
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere");
+      }
+
+      user.password = await hash(password, 8);
+    }
+
+    await knex("users")
+      .where({ id: user_id })
+      .update({ name, email, password: user.password });
+
+    user.updated_at = new Date();
+
+    return response.json({ ...user });
   }
 }
 
